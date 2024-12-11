a0e6b2dccdb2b950614b6db861c9c5f254d70d9f
Sidhart Krishnan <sidhartkrishnan@gmail.com>
2024-12-10 17:55:05 -0800

    Updated SHA256 checksum to match Github release download

--- Formula/sophy-alpha.rb
+++ Formula/sophy-alpha.rb
@@ -2,7 +2,7 @@ class SophyAlpha < Formula
     desc "A simple tool that prints 'Hello World'"
     homepage "https://github.com/SidhartK/sophy"
     url "https://github.com/SidhartK/sophy/archive/refs/tags/v0.1.1-alpha.tar.gz"
-    sha256 "ebc21a3ee2ffb163943db036a0d56a0d1c6d677a4b742502debebeac8aa3f0a5"
+    sha256 "91360b445b653be1375d00295fc64e6c65a7b3d88085110d90b89226fcbcb9eb"
     version "0.1.1-alpha"
   
     def install
