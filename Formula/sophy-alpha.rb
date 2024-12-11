c0ddd70d8c19a7f138712b9540293f4f374726ee
Sidhart Krishnan <sidhartkrishnan@gmail.com>
2024-12-10 17:23:21 -0800

    Add sophy-alpha formula for pre-release v0.1.0-alpha

---
+++ Formula/sophy-alpha.rb
@@ -0,0 +1,12 @@
+class Sophy < Formula
+    desc "A simple tool that prints 'Hello World'"
+    homepage "https://github.com/SidhartK/sophy"
+    url "https://github.com/SidhartK/sophy/archive/refs/tags/v0.1.0-alpha.tar.gz"
+    sha256 "7e016c9b6fca09b0480c257edccb731e60d31d189b92ee4c8bd2bae8a33b68cd  sophy-0.1.0-alpha.tar.gz"
+    version "0.1.0-alpha"
+  
+    def install
+      bin.install "bin/sophy"
+    end
+  end
+  
