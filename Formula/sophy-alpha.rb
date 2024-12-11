ebd19bea1baf1ee793488d86818f221cb75c25e5
Sidhart Krishnan <sidhartkrishnan@gmail.com>
2024-12-10 17:43:11 -0800

    Switching to v0.1.1-alpha

--- Formula/sophy-alpha.rb
+++ Formula/sophy-alpha.rb
@@ -1,9 +1,9 @@
 class SophyAlpha < Formula
     desc "A simple tool that prints 'Hello World'"
     homepage "https://github.com/SidhartK/sophy"
-    url "https://github.com/SidhartK/sophy/archive/refs/tags/v0.1.0-alpha.tar.gz"
-    sha256 "7e016c9b6fca09b0480c257edccb731e60d31d189b92ee4c8bd2bae8a33b68cd  sophy-0.1.0-alpha.tar.gz"
-    version "0.1.0-alpha"
+    url "https://github.com/SidhartK/sophy/archive/refs/tags/v0.1.1-alpha.tar.gz"
+    sha256 "ebc21a3ee2ffb163943db036a0d56a0d1c6d677a4b742502debebeac8aa3f0a5"
+    version "0.1.1-alpha"
   
     def install
       bin.install "bin/sophy"
